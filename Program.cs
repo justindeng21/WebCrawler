ca22ffc63cd705731c37ac1607a74bf22c0c9873
justindeng21 <justin.n.deng@gmail.com>
2025-06-05 18:27:39 -0600

    change number of pages

--- Program.cs
+++ Program.cs
@@ -1,7 +1,7 @@
 ﻿using WebCrawler;
 
 var watch = System.Diagnostics.Stopwatch.StartNew();
-await Crawler.Crawl("https://web-scraping.dev/", 50);
+await Crawler.Crawl("https://web-scraping.dev/", 20);
 watch.Stop();
 var elapsedMs = watch.ElapsedMilliseconds;
 Console.WriteLine($"Scan time: {elapsedMs}ms");
