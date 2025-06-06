949539a2bc10c0870af081495eaee4a5e962a2aa
justindeng21 <justin.n.deng@gmail.com>
2025-06-05 22:21:03 -0600

    update gitignore file

--- Program.cs
+++ Program.cs
@@ -6,7 +6,6 @@ class Program
     {
         string startUrl = "https://finance.yahoo.com/";
         int limit = 20;
-
         var watch = System.Diagnostics.Stopwatch.StartNew();
         await Crawler.CrawlAsync(startUrl, limit);
         watch.Stop();
