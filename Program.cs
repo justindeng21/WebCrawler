08266f3f0b2893f67f4bbad6a288fc5a3be2df4f
justindeng21 <justin.n.deng@gmail.com>
2025-06-05 18:28:31 -0600

    change starting url

--- Program.cs
+++ Program.cs
@@ -1,7 +1,7 @@
 ﻿using WebCrawler;
 
 var watch = System.Diagnostics.Stopwatch.StartNew();
-await Crawler.Crawl("https://web-scraping.dev/", 20);
+await Crawler.Crawl("https://finance.yahoo.com/", 20);
 watch.Stop();
 var elapsedMs = watch.ElapsedMilliseconds;
 Console.WriteLine($"Scan time: {elapsedMs}ms");
